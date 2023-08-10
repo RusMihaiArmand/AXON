25a36290db8a6e8097e49b756a5e273addaf386b
paul_desktop <paul.marcu1@stud.ubbcluj.ro>
2023-08-10 16:32:57 +0300

    [#20] Changed error code to fit format and modified the service implementation based on feedback.

--- src/main/java/ro/axon/dot/service/EmployeeRemainingDaysOffService.java
+++ src/main/java/ro/axon/dot/service/EmployeeRemainingDaysOffService.java
@@ -5,62 +5,73 @@ import org.springframework.stereotype.Service;
 import ro.axon.dot.domain.*;
 import ro.axon.dot.exceptions.BusinessErrorCode;
 import ro.axon.dot.exceptions.BusinessException;
-import ro.axon.dot.mapper.EmployeeMapper;
-import ro.axon.dot.model.EmployeeDetailsListItem;
 import ro.axon.dot.model.RemainingDaysOff;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
-import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeRemainingDaysOffService {
 
     private final EmployeeRepository employeeRepository;
-    private final LegallyDaysOffService legallyDaysOffService;
+//    private final LegallyDaysOffService legallyDaysOffService;
     private final LeaveRequestRepository leaveRequestRepository;
+
+//    private boolean isWeekend(LocalDate date) {
+//        return date.getDayOfWeek().toString().equals("SATURDAY") || date.getDayOfWeek().toString().equals("SUNDAY");
+//    }
+//
+//    private Integer countDaysOffPerRequest(LeaveRequestEty leaveRequest) {
+//        List<LocalDate> legalDaysOffInCurrentYear;
+//        Integer spentDaysOffInRequest = 0;
+//        try {
+//            legalDaysOffInCurrentYear = legallyDaysOffService.getOffDays(null, List.of(String.valueOf(Calendar.getInstance().get(Calendar.YEAR)))).getDays().stream().map(offDay -> offDay.getDate().toLocalDate()).toList();
+//        } catch (NullPointerException e) {
+//            legalDaysOffInCurrentYear = new ArrayList<>();
+//        }
+//        for (LocalDate date = leaveRequest.getStartDate(); !date.isAfter(leaveRequest.getEndDate()); date = date.plusDays(1)) {
+//            if (!(isWeekend(date))) {
+//                if (!legalDaysOffInCurrentYear.contains(date)) {
+//                    spentDaysOffInRequest++;
+//                }
+//            }
+//        }
+//        return spentDaysOffInRequest;
+//    }
 
     public RemainingDaysOff getEmployeeRemainingDaysOff(String employeeId) {
         var remainingDaysOff = new RemainingDaysOff();
-        EmployeeEty employeeEty;
+        Optional<EmployeeEty> employeeOptionalEty;
 
-        try {
-            employeeEty = employeeRepository.findAll().stream().filter(e -> e.getId().equals(employeeId)).toList().get(0);
-        } catch (Exception e) {
+        employeeOptionalEty = employeeRepository.findById(employeeId);
+
+        if (employeeOptionalEty.isEmpty())
             throw new BusinessException(BusinessException.BusinessExceptionElement.builder().errorDescription(BusinessErrorCode.EMPLOYEE_NOT_FOUND).build());
-        }
 
-        EmployeeDetailsListItem employee = EmployeeMapper.INSTANCE.mapEmployeeEtyToEmployeeDto(employeeEty);
+        EmployeeEty employee = employeeOptionalEty.get();
 
-        if (employee.getTotalVacationDays() == 0)
+        Integer currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        Optional<Integer> totalDaysOff = employee.getEmpYearlyDaysOff().stream().filter(daysOffEntry -> daysOffEntry.getYear().equals(currentYear)).findFirst().map(EmpYearlyDaysOffEty::getTotalNoDays);
+
+        if (totalDaysOff.isEmpty())
             throw new BusinessException(BusinessException.BusinessExceptionElement.builder().errorDescription(BusinessErrorCode.YEARLY_DAYS_OFF_NOT_SET).build());
 
-        remainingDaysOff.setRemainingDays(employee.getTotalVacationDays());
-
-        String currentYear = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
-        List<LocalDate> legalDaysOffInCurrentYear;
-        try {
-            legalDaysOffInCurrentYear = legallyDaysOffService.getOffDays(null, List.of(currentYear)).getDays().stream().map(offDay -> offDay.getDate().toLocalDate()).toList();
-        } catch (NullPointerException e) {
-            legalDaysOffInCurrentYear = new ArrayList<>();
-        }
+        remainingDaysOff.setRemainingDays(totalDaysOff.get());
 
         QLeaveRequestEty root = QLeaveRequestEty.leaveRequestEty;
         LeaveRequestQuery query = new LeaveRequestQuery();
-        Iterable<LeaveRequestEty> approvedVacationLeaveRequests = leaveRequestRepository.findAll(root.employee.id.eq(employee.getId()).and(query.withStatus(LeaveRequestEtyStatusEnum.APPROVED).withType(LeaveRequestEtyTypeEnum.VACATION).build()));
+        Iterable<LeaveRequestEty> approvedVacationLeaveRequests = leaveRequestRepository.findAll(root.employee.id.eq(employee.getId())
+                .and(query.withStatus(LeaveRequestEtyStatusEnum.APPROVED).withType(LeaveRequestEtyTypeEnum.VACATION).build())
+                .or(query.withStatus(LeaveRequestEtyStatusEnum.PENDING).withType(LeaveRequestEtyTypeEnum.VACATION).build()));
 
-        for (LeaveRequestEty leaveRequest : approvedVacationLeaveRequests) {
-            for (LocalDate date = leaveRequest.getStartDate(); !date.isAfter(leaveRequest.getEndDate()); date = date.plusDays(1)) {
-                if (!(date.getDayOfWeek().toString().equals("SATURDAY") || date.getDayOfWeek().toString().equals("SUNDAY"))) {
-                    if (!legalDaysOffInCurrentYear.contains(date)) {
-                        remainingDaysOff.setRemainingDays(remainingDaysOff.getRemainingDays() - 1);
-                    }
-                }
-            }
-        }
+        Integer spentDaysOff;
+        //  spentDaysOff = StreamSupport.stream(approvedVacationLeaveRequests.spliterator(), false).map(this::countDaysOffPerRequest).reduce(0, Integer::sum);
 
+        spentDaysOff = StreamSupport.stream(approvedVacationLeaveRequests.spliterator(), false).map(request -> ChronoUnit.DAYS.between(request.getStartDate(), request.getEndDate())).reduce(0L, Long::sum).intValue();
+        remainingDaysOff.setRemainingDays(remainingDaysOff.getRemainingDays() - spentDaysOff);
         return remainingDaysOff;
     }
 }
