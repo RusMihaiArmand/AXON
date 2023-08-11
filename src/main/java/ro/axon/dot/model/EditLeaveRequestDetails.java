f804555fb36e61e9485f37a0a6f8a846628c4c7f
Ielciu Cristian <ielciu.cristian01@gmail.com>
2023-08-11 11:12:03 +0300

    [Task9] Changes based on review

--- src/main/java/ro/axon/dot/model/EditLeaveRequestDetails.java
+++ src/main/java/ro/axon/dot/model/EditLeaveRequestDetails.java
@@ -1,15 +1,22 @@
 package ro.axon.dot.model;
 
 import java.time.LocalDate;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import ro.axon.dot.domain.LeaveRequestEtyTypeEnum;
+import ro.axon.dot.validation.DateRangeConstraint;
 
 @Data
+@DateRangeConstraint
 public class EditLeaveRequestDetails {
 
   private LocalDate startDate;
   private LocalDate endDate;
+  @NotNull(message = "Leave Request type cannot be null")
   private LeaveRequestEtyTypeEnum type;
+  @Size(max = 255, message = "Description cannot exceed 255 characters")
   private String description;
+  @NotNull(message = "Version cannot be null")
   private Long v;
 }
