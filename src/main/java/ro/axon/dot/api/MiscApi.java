1e91e5009ada116142f6eb670c511b224a3a0eb1
Ancuta03 <ancachis38@gamil.com>
2023-08-22 11:55:54 +0300

    Rezolvarea modificarilor cerute.

--- src/main/java/ro/axon/dot/api/MiscApi.java
+++ src/main/java/ro/axon/dot/api/MiscApi.java
@@ -1,11 +1,15 @@
 package ro.axon.dot.api;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ro.axon.dot.model.EmployeeRolesList;
+import ro.axon.dot.model.LegallyDaysOffList;
+import ro.axon.dot.service.LegallyDaysOffService;
 import ro.axon.dot.service.RolesService;
 
 @RestController
@@ -14,10 +18,19 @@ import ro.axon.dot.service.RolesService;
 public class MiscApi {
 
   private final RolesService rolesService;
+  private final LegallyDaysOffService legallyDaysOffService;
+
   @GetMapping("roles")
   public ResponseEntity<EmployeeRolesList> getEmployeeRolesList() {
 
     return ResponseEntity.ok(rolesService.getEmployeeRoles());
+  }
+
+  @GetMapping("legally-days-off")
+  public ResponseEntity<LegallyDaysOffList> getDaysOff(@RequestParam("periods") List<String> periods,
+      @RequestParam("years") List<String> years) {
+
+    return ResponseEntity.ok(legallyDaysOffService.getOffDays(periods, years));
   }
 
 }
